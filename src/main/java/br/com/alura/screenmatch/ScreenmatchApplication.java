19109335c7d687e56659a5a1c927b2d2d0ec63c3
DESKTOP-0CCT33S\KARIBIS <karine@victsoft.com.br>
2025-06-24 17:46:26 -0300

    inicio projeto spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.models.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String chave = "f5feffed";
+		String endereco = "https://www.omdbapi.com/?t=gilmore+girls" + "&apikey=" + chave;
+
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados(endereco);
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
